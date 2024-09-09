d9db58711abceaffa41df2995a382f45fef32c50
Ashwani Kumar <102874884+41611018@users.noreply.github.com>
2024-09-09 16:24:42 +0530

    Time: 8 ms (63.71%), Space: 9 MB (41.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       int freqtable[256] = {0};
-        for(int i=0 ;i<s.length();i++){
+        int freqtable[256] = {0}; //step 0 completed
+        //step 1 : increase count of string s
+        for(int i=0 ; i<s.length(); i++){
             freqtable[s[i]]++;
         }
-        for(int i=0;i<t.length();i++){
+        //step 2 : subtract the count
+        for(int i=0 ; i<t.length();i++){
             freqtable[t[i]]--;
-        }
-        for(int i=0 ; i<256 ; i++){
+        } 
+        //step 3 : check is freqtable count is 0 or not
+        for(int i=0;i<256;i++){
             if(freqtable[i]!=0){
                 return false;
             }
-        }
+        } 
         return true;
     }
 };
